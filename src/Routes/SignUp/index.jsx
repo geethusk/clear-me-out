8dcc3b65d7e91653202566308b3bba1438ab01f9
geethu <geethukallada1@gmail.com>
2021-12-04 11:51:35 +0530

    usestate definision

--- src/Routes/SignUp/index.jsx
+++ src/Routes/SignUp/index.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import "./Style.css"
+import { useState } from 'react';
 
 const SignUp = () => {
+    const [formData,setFormData]=useState({
+        fullName:"",
+        email:"",
+        password:"",
+        confirmPassword:"",
+    });
+        const {fullName,email,password,confirmPassword}=formData;
     return (
-        <div>
-            <h1>Sign Up Here</h1>
+        <div className="main-container">
+            <div className="sub-container">
+            <h1> Sign Up</h1>
+            <form>
+                <div className="input-field">
+                    <input type="text" />
+                    <div className="form-label">
+                        Full Name
+                    </div>
+                </div>
+                <div className="input-field">
+                    <input type="text" />
+                    <div className="form-label">
+                        email
+                    </div>
+                </div>
+                <div className="input-field">
+                    <input type="text" />
+                    <div className="form-label">
+                        Password
+                    </div>
+                </div>
+                <div className="input-field">
+                    <input type="text" />
+                    <div className="form-label">
+                        confirm Password
+                    </div>
+                </div>
+                <button>Sign Up</button>
+            </form>
+        </div>
         </div>
     )
 }
